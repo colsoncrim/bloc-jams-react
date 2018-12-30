8dbbd9fc535ec01dffd419f772ef4ecc95a76adf
colsoncrim <colsonscrim@gmail.com>
2018-12-30 14:10:54 -0800

    Checkpoint 7 complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,21 +10,44 @@ class Album extends Component {
    });
 
    this.state = {
-     album: album
+     album: album,
+     currentSong: album.songs[0],
+     isPlaying: false
    };
+
+   this.audioElement = document.createElement('audio');
+   this.audioElement.src = album.songs[0].audioSrc;
  }
+
+ play() {
+   this.audioElement.play();
+   this.setState({ isPlaying: true });
+ }
+
+ pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+ }
+
+ setSong(song) {
+   this.audioElement.src = song.audioSrc;
+   this.setState({ currentSong: song });
+ }
+
+ handleSongClick(song) {
+   const isSameSong = this.state.currentSong === song;
+   if (this.state.isPlaying && isSameSong) {
+     this.pause();
+   } else {
+     if (!isSameSong) { this.setSong(song); }     
+     this.play();
+   }
+ }
+
 
    render() {
      return (
        <section className="album">
-        {
-          this.state.album.songs.map( (song, index) =>
-            <tr>
-             <td>{song.title}</td>
-           </tr> )
-
-
-        }
          <section id="album-info">
            <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
            <div className="album-details">
@@ -40,7 +63,11 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
-
+            {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+              <td>{song.title}</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </section>
